95bfde10c142dba52f021f9680c73402b6da4fc5
BrendonLH <Brendonhampton1988@gmail.com>
2020-11-17 23:51:53 -0800

    added some styling and basic scaffolding to the app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './header/header.js';
+import Main from './main/main.js';
+import Footer from './footer/footer.js';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Main/>
+
     </div>
   );
 }
---
+++ src/main/main.js
@@ -0,0 +1,13 @@
+import { Navbar } from 'react-bootstrap';
+
+function main() {
+    return (
+    <>
+        <Navbar className='Navbar' bg='dark' variant="dark">
+            <Navbar.Brand>To Do List Manager</Navbar.Brand>
+        </Navbar>  
+    </>
+    )
+}
+
+export default main;
