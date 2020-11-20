416f811375e56c63a28cb35795910c82840cccfb
BrendonLH <Brendonhampton1988@gmail.com>
2020-11-20 11:12:27 -0800

    got the form to submit the proper data to the main.js file and then send it to the list

--- src/main/form.js
+++ src/main/form.js
@@ -11,25 +11,23 @@ function TodoForm(props) {
             ...task, 
             [e.target.name]: e.target.value,
         };
-        console.log(task)
         updateTasks(taskUpdate);
     }
     
-    console.log(task);
+    console.log('state of the added item',task);
 
     function onSubmit(e) {
         e.preventDefault();
-        // e.target.reset()
-        console.log('onSubmit');
-        props.formHandler(task)
-        updateTasks({})
+        e.target.reset();
+        props.formHandler(task);
+        updateTasks({});
     }
     return(
     <>
         <Form onSubmit={onSubmit}>
             <Form.Group controlId="formBasicEmail">
                 <Form.Label>Add To Do Item</Form.Label>
-                <Form.Control onChange={handleTask} name="itemName" type="text" placeholder="Name of Item" />
+                <Form.Control required={true} onChange={handleTask} name="itemName" type="text" placeholder="Name of Item" />
                 <Form.Text className="text-muted">
                     What is the name of your to do?
                 </Form.Text>
@@ -37,7 +35,7 @@ function TodoForm(props) {
 
             <Form.Group controlId="formBasicPassword">
                 <Form.Label>Assigned To</Form.Label>
-                <Form.Control onChange={handleTask} name="assignedTo" type="Text" placeholder="Assignee" />
+                <Form.Control required={true} onChange={handleTask} name="assignedTo" type="Text" placeholder="Assignee" />
                 <Form.Text className="text-muted">
                     Who will be assigned this todo?
                 </Form.Text>
@@ -45,7 +43,7 @@ function TodoForm(props) {
 
             <Form.Group controlId="formBasicRange">
                 <Form.Label>Difficulty</Form.Label>
-                <Form.Control name="difficulty" onChange={handleTask} type="range" />
+                <Form.Control defaultValue={0} required={true} name="difficulty" onChange={handleTask} type="range" />
                 <Form.Text className="text-muted">
                     How difficult is the task?
                 </Form.Text>
