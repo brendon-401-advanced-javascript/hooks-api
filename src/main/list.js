7f4a7b4a7481c93f1b9262cfb7eccbff08838226
BrendonLH <Brendonhampton1988@gmail.com>
2020-12-03 18:34:36 -0800

    added more styling so the page looks correct

--- src/main/list.js
+++ src/main/list.js
@@ -1,6 +1,8 @@
 // import { Form, Button} from 'react-bootstrap';
 import './main.scss';
 import useAxios from 'axios-hooks';
+import { Toast } from 'react-bootstrap';
+import ToastHeader from 'react-bootstrap/ToastHeader';
 
 
 function TodDoList(props) {
@@ -8,6 +10,7 @@ function TodDoList(props) {
     const [{ data, loading, error }, refetch] = useAxios(
     'https://auth-app-bh.herokuapp.com/api/v1/todo'
     );
+    console.log(data);
 
     if (loading) return <p>Loading...</p>
     if (error) return <p>Error!</p>
@@ -15,10 +18,24 @@ function TodDoList(props) {
     return (
         
         <>
+            
+            <div className='List'>
             <h4>Tasks to be Completed</h4>  
-            <div>
+                {
+                data.results.map((item) => {
+                    return<Toast>
+                        <Toast.Header>
+                        <img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" />
+                        <strong className="mr-auto">{JSON.stringify(item.complete)}</strong>
+                        </Toast.Header>
+                        <Toast.Body>{item.text}</Toast.Body>
+                        <Toast.Body>Difficulty: {item.difficulty}</Toast.Body>
+                    </Toast>
+
+                })
+                }
                 <button onClick={refetch}>refresh</button>
-                <pre>{JSON.stringify(data, null, 2)}</pre>
+                
             </div>
         </>
     )
